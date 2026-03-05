056cb484f58aaaba22e3f7e7dfefab9cdee2d2ea
Kauan Da Silva <kauan@sunfieldmarketing.com>
2026-03-05 01:11:07 -0500

    Update Tally form height to 762

--- src/App.tsx
+++ src/App.tsx
@@ -105,7 +105,7 @@ const Hero = () => {
                     data-tally-src="https://tally.so/embed/gD5BpJ?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
                     loading="lazy"
                     width="100%"
-                    height="747"
+                    height="762"
                     frameBorder="0"
                     marginHeight={0}
                     marginWidth={0}
